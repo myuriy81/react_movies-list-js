aa10b4bd22d275a0acf4c3301cf4ab6dbb2010df
Yurii Mosin <m.yuriy@icloud.com>
2024-11-14 20:07:34 +0200

    add solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -12,7 +12,7 @@ export const MovieCard = ({ movie }) => (
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src={movie.imdbUrl} alt="imdb" />
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
